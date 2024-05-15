d44497160236259c0a45f460d590ab6af26287f0
Cossta07 <samuellinker46@gmail.com>
2024-05-15 16:15:05 -0400

    add bullet class

---
+++ src/javascript/Bullet.js
@@ -0,0 +1,14 @@
+class Bullet {
+    constructor(canvas, x, y, velocity, bullteColor){
+        this.canvas = canvas;
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+        this.bullteColor = bullteColor;
+
+        this.width = 5;
+        this.height = 20;
+
+    }
+    
+}
