456d06e969604ee9b9aa81c3667cb7e84100b95f
Cossta07 <samuellinker46@gmail.com>
2024-05-15 16:37:45 -0400

    Method that checks whether a collision has occurred

--- src/javascript/Bullet.js
+++ src/javascript/Bullet.js
@@ -1,10 +1,10 @@
 class Bullet {
-    constructor(canvas, x, y, velocity, bullteColor){
+    constructor(canvas, x, y, velocity, bulletColor){
         this.canvas = canvas;
         this.x = x;
         this.y = y;
         this.velocity = velocity;
-        this.bullteColor = bullteColor;
+        this.bulletColor = bulletColor;
 
         this.width = 5;
         this.height = 20;
@@ -15,5 +15,17 @@ class Bullet {
         ctx.fillStyle = this.bulletColor;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
+    collideWith(sprite) {
+        if(
+            this.x + this.width > sprite.x &&
+            this.x < sprite.x + sprite.width &&
+            this.y + this.height > sprite.y &&
+            this.y < sprite.y + sprite.height
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }
