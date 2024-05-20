151d832a6e9e85f26a6537de31f2bed9d86f494e
Cossta07 <samuellinker46@gmail.com>
2024-05-20 17:03:53 -0400

    add draw, move and colllideWith in Player class

--- src/javascript/Player.js
+++ src/javascript/Player.js
@@ -13,5 +13,33 @@ export default class Player{
         this.height = 48;
         this.image = new Image();
         this.image.src = "src/assets/imagens/player.png";
+
+        document.addEventListener('Keydown', this.keyDown); 
+        document.addEventListener('Keyup', this.keyUp);
+
+    }
+    draw(ctx) {
+        if (this.shootPressed) {
+            this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 18);
+        }
+        this.move();
+        this.collideWith();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    move() {
+        if(this.rightPressed){
+            this.x += this.velocity;
+        }else if (this.leftPressed){
+            this.x -= this.velocity;
+        }
+    }
+
+    collideWith(){
+        if(this.x < 0){
+            this.x = 0;
+        }
+        if (this.x > this.canvas.with - this.width){
+            this.x = this.canvas.with - this.width;
+        }
     }
 }
